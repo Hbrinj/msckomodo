3af280f5ad0357cd5958a38ce8a8dd630f856c8a
Houman brinjcargorabi <hbrinjcar@gmail.com>
2017-04-23 20:04:40 +0100

    Sanity commit before going to master
    Signed-off-by: Houman brinjcargorabi <hbrinjcar@gmail.com>

---
+++ komodo/config.h
@@ -0,0 +1,51 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+/* #undef ENABLE_NLS */
+/* #undef HAVE_CATGETS */
+/* #undef HAVE_GETTEXT */
+/* #undef HAVE_LC_MESSAGES */
+/* #undef HAVE_STPCPY */
+/* #undef HAVE_LIBSM */
+/* #undef PACKAGE_LOCALE_DIR */
+/* #undef PACKAGE_DATA_DIR */
+/* #undef PACKAGE_SOURCE_DIR */
+#define LINUX 1
+/* #undef SUNOS */
+#define VERBOSE_D 0
+#define SERIAL_DEBUG 0
+/* #undef SERIAL_SPEED */
+/* #undef SERIAL_PORT */
+/* #undef EMULATOR_PROG */
+/* #undef SETUP_DIR */
+#define DATE "02/04/2017"
+#define YEAR "2017"
+/* #undef SIMPLE_INTERFACE */
+/* #undef USE_INTERNAL */
+/* #undef GTK2 */
+
+/* Name of package */
+#define PACKAGE "KMD"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "1.5.0"
